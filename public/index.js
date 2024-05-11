1cc5c4b57aedae2eabc9b794fc5d6beaef544ecd
Byamukamam <131506113+Byamukamam@users.noreply.github.com>
2024-05-11 13:18:26 +0300

    Add files via upload

--- public/index.js
+++ public/index.js
@@ -1,10 +1,21 @@
-let ison = false
+let ison = false;
+let mobion = false;
 function dropDown(){
     if (!ison){
-        document.getElementById("dropDown").classList.add("hidden-links")
-        ison = true
+        document.getElementById("dropDown").classList.add("hidden-links");
+        ison = true;
     } else {
-        document.getElementById("dropDown").classList.remove("hidden-links")
-        ison = false
+        document.getElementById("dropDown").classList.remove("hidden-links");
+        ison = false;
     } 
+}
+
+function mobileDrop(){
+    if(!mobion){
+        document.getElementById("mobileDrop").classList.add("mobdevice");
+        mobion = true;
+    } else {
+        document.getElementById("mobileDrop").classList.remove("mobdevice");
+        mobion = false;
+    }
 }
